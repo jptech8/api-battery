4d5e9f54ee7c17444b5fdcae47bc027e5fb69588
vengatesan kandasamy <vengatesankandasamy@vengatesans-Mac-mini.local>
2023-10-24 21:26:00 +0530

    ping service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): {name:string;service:string;date:Date;} {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+
+
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getHello ():  {
+    name:string;
+    service:string;
+    date:Date;
+    
+    }{
+    return {name:process.env.APP_NAME,service:"ping service",date:new Date()}
   }
 }
+
+
+
